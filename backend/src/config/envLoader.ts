4bb2f6ed811f6c68be60827541ea03f173c55416
phin-kim <phinehasnjuguna1@gmail.com>
2026-04-04 20:13:55 +0300

    Update envLoader.ts

--- backend/src/config/envLoader.ts
+++ backend/src/config/envLoader.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import fs from 'fs-extra';
 import createLogger from '../utils/logger.js';
+
 const log = createLogger('EnvLoader.ts');
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
